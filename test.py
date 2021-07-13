11edf3080328594f4deac6a3ca5f35e8b879ed88
Harry <harryrichman@github.com>
2021-07-12 22:20:11 -0700

    test

--- test.py
+++ test.py
@@ -2,4 +2,6 @@ import torch
 import dgl
 from datasets.ogbg_dataset_extension import OGBGDatasetExtension
 
-dataset = OGBGDatasetExtension(return_types=['dgl_graph', ])
+datas = OGBGDatasetExtension(return_types=['dgl_graph', 'targets'], name='ogbg-molbace', device='cpu')
+print(datas[0])
+print(datas[0][0].ndata)
